94dc6498b37a17609bf53d5957565a59dcdc5c0c
Rickard Marjanovic <rickard.marjanovic@gmail.com>
2020-12-06 20:23:57 +0100

    Adding test req to insert into database

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -20,4 +20,46 @@ apiController.home = async (req, res) => {
   res.status(200).send('Data')
 }
 
+/** Insert test.
+ *
+ * @param {object} req - request object
+ * @param {object} res - result object
+ */
+apiController.msg = async (req, res) => {
+  console.log(req.body)
+  db.connection.query(
+    'INSERT INTO contactdetails (PhoneNo, Adress, PostalNo, City, Email, IsDeleted) VALUES (?,?,?,?,?,?)',
+    [
+      'test',
+      'testgatan 1',
+      '25250',
+      'Helsinborg',
+      'email@email.com',
+      false
+    ],
+    (error, results) => {
+      if (error) return res.json({ error: error })
+      db.connection.query(
+        'INSERT INTO patient (FirstName, LastName, SocialSecurityNumber, ProofOfIdentification, IdentificationType, CreatedDate, CreatedBy, ContactDetailId, IsDeleted) VALUES (?,?,?,?,?,?,?,?,?)',
+        [
+          'Kalle',
+          'Anka',
+          '350526-1959',
+          true,
+          'Licence',
+          new Date(),
+          1,
+          results.insertId,
+          false
+        ],
+        function (error, results, fields) {
+          if (error) {
+            console.log(error)
+          }
+        }
+      )
+      res.status(200).send(JSON.stringify(results))
+    })
+}
+
 module.exports = apiController
--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -4,5 +4,6 @@ const router = express.Router()
 const apiController = require('../controllers/apiController')
 
 router.get('/', apiController.home)
+router.get('/msg', apiController.msg)
 
 module.exports = router
