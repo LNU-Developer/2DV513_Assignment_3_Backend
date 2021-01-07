36153c9496541a0496fc75c00f11c468496ba573
Rickard Marjanovic <rickard.marjanovic@gmail.com>
2021-01-08 00:44:44 +0100

    Adding queries for male and female

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -19,11 +19,43 @@ db.connect()
 apiController.getPatient = async (req, res) => {
   const ssn = req.params.ssn
   db.connection.query(
-    'SELECT * FROM patient WHERE SocialSecurityNumber = ?', ssn,
+    'SELECT * FROM patient WHERE SocialSecurityNumber = ?',
+    ssn,
     (error, results) => {
       if (error) return res.json({ error: error })
       res.status(200).send(results)
-    })
+    }
+  )
+}
+
+/** Get al Male patients from the database.
+ *
+ * @param {object} req - request object
+ * @param {object} res - result object
+ */
+apiController.getPatientMale = async (req, res) => {
+  db.connection.query(
+    'SELECT * FROM malepatients',
+    (error, results) => {
+      if (error) return res.json({ error: error })
+      res.status(200).send(results)
+    }
+  )
+}
+
+/** Get al Female patients from the database.
+ *
+ * @param {object} req - request object
+ * @param {object} res - result object
+ */
+apiController.getPatientFemale = async (req, res) => {
+  db.connection.query(
+    'SELECT * FROM femalepatients',
+    (error, results) => {
+      if (error) return res.json({ error: error })
+      res.status(200).send(results)
+    }
+  )
 }
 
 /** Get all patients from database.
@@ -37,7 +69,23 @@ apiController.getAllPatients = async (req, res) => {
     (error, results) => {
       if (error) return res.json({ error: error })
       else res.status(200).send(results)
-    })
+    }
+  )
+}
+
+/** Get group patients on city from database.
+ *
+ * @param {object} req - request object
+ * @param {object} res - result object
+ */
+apiController.getCountByCity = async (req, res) => {
+  db.connection.query(
+    'SELECT COUNT(ContactDetailId) AS count, City AS cityName FROM contactdetails GROUP BY City',
+    (error, results) => {
+      if (error) return res.json({ error: error })
+      else res.status(200).send(results)
+    }
+  )
 }
 
 /** Insert patient.
@@ -78,7 +126,8 @@ apiController.addPatient = async (req, res) => {
           }
         )
       }
-    })
+    }
+  )
 }
 
 /** Edit patient.
@@ -90,38 +139,41 @@ apiController.editPatient = async (req, res) => {
   const patientId = req.body.PatientId
   const patient = req.body
   db.connection.query(
-    'SELECT * FROM patient WHERE PatientId = ?', patientId,
+    'SELECT * FROM patient WHERE PatientId = ?',
+    patientId,
     (error, results) => {
       const contactDetailId = results[0].ContactDetailId
       if (error) return res.json({ error: error })
       db.connection.query(
-        `UPDATE contactdetails SET PhoneNo = ?, Adress = ?, PostalNo = ?, City = ?, Email = ? WHERE ContactDetailId = ${contactDetailId}`,
-        [
-          patient.PhoneNo,
-          patient.Adress,
-          patient.PostalNo,
-          patient.City,
-          patient.Email
-        ],
-        (error, results) => {
-          if (error) return res.json({ error: error })
-          else {
-            db.connection.query(
-              `UPDATE patient SET FirstName = ?, LastName = ?, SocialSecurityNumber = ?, IdentificationType = ? WHERE PatientId = ${patientId}`,
-              [
-                patient.FirstName,
-                patient.LastName,
-                patient.SocialSecurityNumber,
-                patient.IdentificationType
-              ],
-              function (error, results) {
-                if (error) return res.json({ error: error })
-                else res.status(200).send('OK')
-              }
-            )
-          }
-        })
-    })
+                `UPDATE contactdetails SET PhoneNo = ?, Adress = ?, PostalNo = ?, City = ?, Email = ? WHERE ContactDetailId = ${contactDetailId}`,
+                [
+                  patient.PhoneNo,
+                  patient.Adress,
+                  patient.PostalNo,
+                  patient.City,
+                  patient.Email
+                ],
+                (error, results) => {
+                  if (error) return res.json({ error: error })
+                  else {
+                    db.connection.query(
+                            `UPDATE patient SET FirstName = ?, LastName = ?, SocialSecurityNumber = ?, IdentificationType = ? WHERE PatientId = ${patientId}`,
+                            [
+                              patient.FirstName,
+                              patient.LastName,
+                              patient.SocialSecurityNumber,
+                              patient.IdentificationType
+                            ],
+                            function (error, results) {
+                              if (error) return res.json({ error: error })
+                              else res.status(200).send('OK')
+                            }
+                    )
+                  }
+                }
+      )
+    }
+  )
 }
 
 /** Soft delete patient.
@@ -134,10 +186,7 @@ apiController.deletePatient = async (req, res) => {
 
   db.connection.query(
     'UPDATE patient SET IsDeleted = ? WHERE `SocialSecurityNumber` = ?',
-    [
-      true,
-      ssn
-    ],
+    [true, ssn],
     function (error, results, fields) {
       if (error) return res.json({ error: error })
     }
